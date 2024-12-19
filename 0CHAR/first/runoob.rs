0cf9cb80b4fbd173dea7706b7b58d6b8e681cb68
Lix <lixinrong4002@gmail.com>
2024-12-19 21:42:59 +0800

    first

---
+++ 0CHAR/first/runoob.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    println!("I want to kill {0}, and kill {0}, again!!!", "ALL OF YOU");
+    println!("{{ I hope it will hurts! }}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RUST_LN
+
+the notes that recording my learning path toward Rust programming language.
+tutourial: "https://www.runoob.com/rust/rust-tutorial.html"
